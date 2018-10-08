b0d52ef7c392333d1611ea18d0aaff080e3e9eab
Michael Trans <mich282q@edu.easj.dk>
2018-10-08 11:28:30 +0200

    learnyounode

---
+++ baby-steps.js
@@ -0,0 +1,7 @@
+/*
+Write a program that accepts one or more numbers as command-line arguments
+  and prints the sum of those numbers to the console (stdout).
+*/
+
+
+console.log(Number(process.argv[2]) + Number(process.argv[3]));
